8ecf32af340ced658136a323fe8bd7cf55dddd09
rogerhdh <hqmhot@hotmail.com>
2023-03-21 15:03:42 +0800

    bug demo

---
+++ Content/JavaScript/Modules/TsTestAnimNotify.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const UE = require("ue");
+class TsTestAnimNotify extends UE.AnimNotify {
+    DebugLog() {
+        console.log("anim notify debug log");
+    }
+}
+exports.default = TsTestAnimNotify;
+//# sourceMappingURL=TsTestAnimNotify.js.map
---
+++ TsProj/Modules/TsTestAnimNotify.ts
@@ -0,0 +1,8 @@
+import * as UE from 'ue'
+
+class TsTestAnimNotify extends UE.AnimNotify {
+    public DebugLog(): void {
+        console.log("anim notify debug log")
+    }
+}
+export default TsTestAnimNotify;
